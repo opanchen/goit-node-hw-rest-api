e12d33284ebd2acfb94d98d5ffd0d8e8c640a811
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-07-22 14:14:16 +0200

    change email provider

--- services/emailSendler.js
+++ services/emailSendler.js
@@ -1,17 +1,28 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const { META_USER, META_PASSWORD } = process.env;
+const { META_USER, META_PASSWORD, NODEMAILER_EMAIL, NODEMAILER_PASSWORD } =
+  process.env;
+
+// const nodemailerConfig = {
+//   host: "smtp.meta.ua",
+//   port: 465, // also can use 25 or 2525, but they aren't secured
+//   secure: true,
+//   // secure: false,
+//   // pull: true,
+//   auth: {
+//     user: META_USER,
+//     pass: META_PASSWORD,
+//   },
+// };
+//
+//* Try to use gmail api instead of simple smtp protocol
 
 const nodemailerConfig = {
-  host: "smtp.meta.ua",
-  port: 465, // also can use 25 or 2525, but they aren't secured
-  secure: true,
-  // secure: false,
-  // pull: true,
+  service: "gmail",
   auth: {
-    user: META_USER,
-    pass: META_PASSWORD,
+    user: NODEMAILER_EMAIL,
+    pass: NODEMAILER_PASSWORD,
   },
 };
 
